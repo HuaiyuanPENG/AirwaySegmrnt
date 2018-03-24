62f97e66864da44f83b608a70e8755f07d6dd993
HuaiyuanPENG <huaiyuanp@gmail.com>
2018-03-24 14:21:13 +0800

    modified CMakeLists

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
+#include "mainwindow.h"
+#include <QApplication>
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
